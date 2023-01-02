6801d82f3820ba0449d617e644e9a3a82b00b0d7
papadoster <papadoster13@gmail.com>
2023-01-02 15:45:05 +0100

    1

--- test/ViewController.swift
+++ test/ViewController.swift
@@ -14,6 +14,23 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func showButton(_ sender: UIButton) {
+        
+        let alertController = UIAlertController(title: "Alert", message: "Achtung!", preferredStyle: .actionSheet)
+        let action = UIAlertAction(title: "ok", style: .default) { (action) in
+//            let text = alertController.textFields?.first?.text
+//            print(text ?? "nil text")
+        }
+        
+//        alertController.addTextField()
+        alertController.addAction(action)
+        self.present(alertController, animated: true )
+        
+    }
+    
+    
+    //MARK: -
+    
+    
 }
 
